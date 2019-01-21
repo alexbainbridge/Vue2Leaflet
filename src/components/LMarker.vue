55d6079eb1eff18ad042aacd281f60a6bfea882b
Alex Bainbridge <alex.bainbridge@tstc.io>
2019-01-21 10:32:51 +0000

    ) !

--- src/components/LMarker.vue
+++ src/components/LMarker.vue
@@ -49,7 +49,7 @@ export default {
     }, this);
     this.mapObject = L.marker(this.latLng, options);
     L.DomEvent.on(this.mapObject, this.$listeners);
-    this.mapObject.on('move', this.latLngSync;
+    this.mapObject.on('move', this.latLngSync);
     propsBinder(this, this.mapObject, this.$options.props);
     this.parentContainer = findRealParent(this.$parent);
     this.parentContainer.addLayer(this, !this.visible);
